917da8146155666c71be43ae0ac60a677504600e
Christian Mathiesen <cm1995@live.dk>
2023-08-04 13:01:56 +0200

    Opdateret form

---
+++ BorgerSide/Data/Migrations/20230803134311_olietankparameters.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BorgerSide.Data.Migrations
+{
+    public partial class olietankparameters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ansoegning",
+                table: "OlietankFormular",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fabrikationsår",
+                table: "OlietankFormular",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Registreringsnummer",
+                table: "OlietankFormular",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Sloejfningsdato",
+                table: "OlietankFormular",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sloejfningsmetode",
+                table: "OlietankFormular",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Stoerrelse",
+                table: "OlietankFormular",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Typegodkendelsesnummer",
+                table: "OlietankFormular",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ansoegning",
+                table: "OlietankFormular");
+
+            migrationBuilder.DropColumn(
+                name: "Fabrikationsår",
+                table: "OlietankFormular");
+
+            migrationBuilder.DropColumn(
+                name: "Registreringsnummer",
+                table: "OlietankFormular");
+
+            migrationBuilder.DropColumn(
+                name: "Sloejfningsdato",
+                table: "OlietankFormular");
+
+            migrationBuilder.DropColumn(
+                name: "Sloejfningsmetode",
+                table: "OlietankFormular");
+
+            migrationBuilder.DropColumn(
+                name: "Stoerrelse",
+                table: "OlietankFormular");
+
+            migrationBuilder.DropColumn(
+                name: "Typegodkendelsesnummer",
+                table: "OlietankFormular");
+        }
+    }
+}
--- BorgerSide/Models/OlietankFormular.cs
+++ BorgerSide/Models/OlietankFormular.cs
@@ -13,7 +13,19 @@ namespace BorgerSide.Models
         [Required]
         public string Ejer { get; set; }
         [Required]
+        public string Ansoegning { get; set; }
+        [Required]
+        public int Registreringsnummer { get; set; }
+        public int Typegodkendelsesnummer { get; set; }
+        public int Fabrikationsår { get; set; }
+        public int Stoerrelse { get; set; }
+        [Required]
+        public DateTime Sloejfningsdato { get; set; }
+        [Required]
+        public string Sloejfningsmetode { get; set; }
+        [Required]
         public string Forurening { get; set; }
+
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
         public OlietankFormular()
